3c60fdd76c6470f66ddeae26353b85679fc5728b
Hemant Sindhyan <kumarhemant122156@gmail.com>
2025-10-22 01:52:35 +0530

    feat(auth): implement user login route with JWT token and cookie handling

--- src/middlewares/loggedStatus.js
+++ src/middlewares/loggedStatus.js
@@ -1,36 +1,34 @@
 import { verifyAccessToken, verifyRefreshToken } from "../utils/generateToken";
 
-async function preventLoggedInAccess(req,res,next) {
- 
-try {
+async function preventLoggedInAccess(req, res, next) {
+  try {
     const refreshToken = req.cookies.refreshToken;
- const accessToken = req.cookies.accessToken;
- if(!refreshToken|| !accessToken) {
-    req.clearCookie('refreshToken',{
-        httpOnly:true,
-        secure:true,
-        sameSite:'strict'
-    })
-     req.clearCookie('accessToken',{
-        httpOnly:true,
-        secure:true,
-        sameSite:'strict'
-    })  
-    next();
- };
- const decodedAccess =  verifyAccessToken(accessToken);
- if(!decodedAccess.success){
-    const decodedRefresh =verifyRefreshToken(refreshToken);
-    if(decodedRefresh.success)
-    {
-        const token ="dcdwfwefewf";
-        console(token);
+    const accessToken = req.cookies.accessToken;
+    if (!refreshToken || !accessToken) {
+      req.clearCookie("refreshToken", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+      });
+      req.clearCookie("accessToken", {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+      });
+      next();
     }
- }
- next()
-} catch (error) {
+    const decodedAccess = verifyAccessToken(accessToken);
+    if (!decodedAccess.success) {
+      const decodedRefresh = verifyRefreshToken(refreshToken);
+      if (decodedRefresh.success) {
+        const token = "dcdwfwefewf";
+        console(token);
+      }
+    }
+    next();
+  } catch (error) {
     console(error);
-}
+  }
 }
 
 export default preventLoggedInAccess;
